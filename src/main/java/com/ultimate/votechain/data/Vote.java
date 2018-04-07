77d4287f163bfb4e1cb2a513d2b33eebe073637f
jaredltrejo <36608766+jaredltrejo@users.noreply.github.com>
2018-04-07 16:25:17 -0400

    basic vote class

--- src/main/java/com/ultimate/votechain/data/Vote.java
+++ src/main/java/com/ultimate/votechain/data/Vote.java
@@ -2,5 +2,17 @@ package com.ultimate.votechain.data;
 
 public class Vote
 {
+//contain position and person, set methods:
+    private String position;
+    private String candidate;
 
+    public void setPosition(String newPosition)
+    {
+        position = newPosition;
+    }
+    public void setCandidate(String newCandidate)
+    {
+        candidate = newCandidate;
+    }
+    
 }
