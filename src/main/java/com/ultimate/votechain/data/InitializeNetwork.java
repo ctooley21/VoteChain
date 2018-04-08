01e80d264fc8be53867bc6c35c0f3351c33ebf8f
BryanLikesToProgram <bryan.racic@angrysquirrels.com>
2018-04-08 01:31:50 -0400

    Networking Update
    Added a networking class, the peer to peer system begins!

---
+++ src/main/java/com/ultimate/votechain/data/InitializeNetwork.java
@@ -0,0 +1,72 @@
+package com.ultimate.votechain.data;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class InitializeNetwork{
+	//10.48.238.98 9001
+	
+	static ServerSocket HOSTSOCK = null;	
+    static Socket clientSock = null;
+    static ServerThread threadInstance = null;
+    	//Can only be one instance of a port per thread
+    
+    String line;
+    Scanner is;
+    PrintStream os;
+    
+    private InitializeNetwork() {
+    	Initalize();
+    }
+    
+    private static void Initalize() {
+
+	    try {
+	    	HOSTSOCK = new ServerSocket(9001);
+	    }catch(IOException e) {
+	    	System.out.println("Server Error");
+	    	e.printStackTrace();
+	    }
+	    System.out.println("Server Listening on port" + HOSTSOCK +"....");
+	    
+	    while(true) {
+	    	try {
+	    		clientSock = HOSTSOCK.accept();
+	    		System.out.println("Connection Established with "+clientSock+"!");
+	    		threadInstance = new ServerThread();
+	    		threadInstance.start();
+	    		threadInstance.setClientThread(clientSock);
+	    	}catch(Exception e) {
+	    		
+	    	}
+	    }
+	}
+    public static class ServerThread extends Thread{
+    	String inpt = null;
+    	Scanner inSocket = null;
+    	Scanner outSocket = null;
+    	Socket tClientSocket = null;
+    	
+		private ServerThread() {
+			
+		}
+		private void setClientThread(Socket clientSock) {
+			tClientSocket = clientSock;
+		}
+		public void run() {
+			//Must be public as is apart of thread
+			
+			try {
+				inSocket = new Scanner(new InputStreamReader(tClientSocket.getInputStream()));
+				outSocket = new Scanner((Readable) tClientSocket.getOutputStream());
+					//Kind of janky might need to change to PrintWriter
+			}catch(IOException e){
+				inpt=inSocket.nextLine();
+					//Might need to change line to just next
+				System.out.println("Error on client port");
+			}	
+		}
+    }
+}
+
