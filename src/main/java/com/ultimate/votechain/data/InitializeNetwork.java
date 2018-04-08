6776ea8d563d4162fa7627d04e6e23f4785fb5ff
BryanLikesToProgram <bryan.racic@angrysquirrels.com>
2018-04-08 02:36:07 -0400

    Working Networking Initialization

--- src/main/java/com/ultimate/votechain/data/InitializeNetwork.java
+++ src/main/java/com/ultimate/votechain/data/InitializeNetwork.java
@@ -4,84 +4,67 @@ import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 
-public class InitializeNetwork
-{
-    //10.48.238.98 9001
+public class InitializeNetwork{
+	//Bryan 10.48.238.98 9001
+	//Bryan2 10.47.29.179
 	
 	static ServerSocket HOSTSOCK = null;	
     static Socket clientSock = null;
     static ServerThread threadInstance = null;
-    //Can only be one instance of a port per thread
+    	//Can only be one instance of a port per thread
     
     String line;
     Scanner is;
     PrintStream os;
     
-    private InitializeNetwork()
-    {
+    private InitializeNetwork() {
     	Initalize();
     }
     
-    private static void Initalize()
-    {
-	    try
-        {
+    private static void Initalize() {
+
+	    try {
 	    	HOSTSOCK = new ServerSocket(9001);
-	    }
-	    catch(IOException e)
-        {
+	    }catch(IOException e) {
 	    	System.out.println("Server Error");
 	    	e.printStackTrace();
 	    }
-	    System.out.println("Server Listening on port" + HOSTSOCK +"....");
+	    System.out.println("Server Listening on " + HOSTSOCK +"....");
 	    
-	    while(true)
-        {
-	    	try
-            {
+	    while(true) {
+	    	try {
 	    		clientSock = HOSTSOCK.accept();
 	    		System.out.println("Connection Established with "+clientSock+"!");
 	    		threadInstance = new ServerThread();
 	    		threadInstance.start();
 	    		threadInstance.setClientThread(clientSock);
-	    	}
-	    	catch(Exception e)
-            {
+	    	}catch(Exception e) {
 	    		
 	    	}
 	    }
 	}
-
-    public static class ServerThread extends Thread
-    {
+    public static class ServerThread extends Thread{
     	String inpt = null;
     	Scanner inSocket = null;
     	Scanner outSocket = null;
     	Socket tClientSocket = null;
     	
-		private ServerThread()
-        {
+		private ServerThread() {
 			
 		}
-
-		private void setClientThread(Socket clientSock)
-        {
+		private void setClientThread(Socket clientSock) {
 			tClientSocket = clientSock;
 		}
-		
-		public void run()
-        {
+		public void run() {
 			//Must be public as is apart of thread
-			try
-            {
+			
+			try {
 				inSocket = new Scanner(new InputStreamReader(tClientSocket.getInputStream()));
 				outSocket = new Scanner((Readable) tClientSocket.getOutputStream());
-				//Kind of janky might need to change to PrintWriter
-			}
-			catch(IOException e)
-            {
+					//Kind of janky might need to change to PrintWriter
+			}catch(IOException e){
 				inpt=inSocket.nextLine();
-				//Might need to change line to just next
+					//Might need to change line to just next
 				System.out.println("Error on client port");
 			}	
 		}
